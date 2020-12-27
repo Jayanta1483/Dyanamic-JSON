b08bebc00b844cd472196cdee03c300f24697e7f
Jayanta <nemojoy2001@gmail.com>
2020-12-27 18:22:42 +0530

    starting dynamic json file creating from datadase

---
+++ dynamic-json/process.php
@@ -0,0 +1,18 @@
+<?php
+$connect = new mysqli("localhost", "root", "", "jay_db");
+
+$select = "";
+
+
+?>
+<!DOCTYPE html>
+<html lang="en-IN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>json-process</title>
+</head>
+<body>
+    
+</body>
+</html>
