637ea4b7cfd88b5993a88862fae05870a123a143
Jayanta <nemojoy2001@gmail.com>
2020-12-27 19:05:08 +0530

    completed dynamic json file from database

--- dynamic-json/process.php
+++ dynamic-json/process.php
@@ -1,18 +1,21 @@
 <?php
 $connect = new mysqli("localhost", "root", "", "jay_db");
-
-$select = "select * from persons";
+date_default_timezone_set("Asia/Kolkata");
+$select = "select * from persons order by firstname";
 $query = $connect->query($select);
 // $outcome = "";
 // while($result = $query->fetch_assoc()){
 //    echo $result;
 // }
 $result = $query->fetch_all(MYSQLI_ASSOC);
-$outcome = json_encode($result);
+$json_data = json_encode($result, JSON_PRETTY_PRINT);
+$json_file = "my_".date("d-m-y h-i-sa").".json";
 
+if(file_put_contents("my-json/".$json_file, $json_data, )){
+    echo $json_data." File Created.";
+}
 
-
-print_r($outcome);
+//print_r($json_data);
 
 
 ?>
