094b8a82e7df711a0f700c02ba1965f25d5beab2
Jayanta <nemojoy2001@gmail.com>
2020-12-27 18:47:05 +0530

    json encoded data from database

--- dynamic-json/process.php
+++ dynamic-json/process.php
@@ -1,7 +1,18 @@
 <?php
 $connect = new mysqli("localhost", "root", "", "jay_db");
 
-$select = "";
+$select = "select * from persons";
+$query = $connect->query($select);
+// $outcome = "";
+// while($result = $query->fetch_assoc()){
+//    echo $result;
+// }
+$result = $query->fetch_all(MYSQLI_ASSOC);
+$outcome = json_encode($result);
+
+
+
+print_r($outcome);
 
 
 ?>
